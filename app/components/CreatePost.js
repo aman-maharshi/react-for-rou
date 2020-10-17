f1686f83f505ed2318d1c40336e0dfde29c5c17e
aman-maharshi <amanm4harshi@gmail.com>
2020-10-17 10:08:19 +0530

    create new post functionality added

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -1,14 +1,17 @@
 /* AXIOS - CREATING NEW POST */
-import React from "react"
+import React, { useState } from "react"
 import Container from "./Container"
 import Axios from "axios"
 
 function CreatePost() {
+    const [postTitle, setPostTitle] = useState()
+    const [postBody, setPostBody] = useState()
+
     async function handleSubmit(e) {
         e.preventDefault()
 
         try {
-            await Axios.post("/create-post", { title: "Test Title", body: "Test Body", token: localStorage.getItem("goSocialToken") })
+            await Axios.post("/create-post", { title: postTitle, body: postBody, token: localStorage.getItem("goSocialToken") })
             console.log("New Post Created")
         } catch (e) {
             console.log(e.response.data)
@@ -22,14 +25,14 @@ function CreatePost() {
                     <label htmlFor="post-title" className="text-muted mb-1">
                         <small>Title</small>
                     </label>
-                    <input autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+                    <input onChange={e => setPostTitle(e.target.value)} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="post-body" className="text-muted mb-1 d-block">
                         <small>Body Content</small>
                     </label>
-                    <textarea name="body" id="post-body" className="body-content tall-textarea form-control" type="text"></textarea>
+                    <textarea onChange={e => setPostBody(e.target.value)} name="body" id="post-body" className="body-content tall-textarea form-control" type="text"></textarea>
                 </div>
 
                 <button className="btn btn-primary">Save New Post</button>
